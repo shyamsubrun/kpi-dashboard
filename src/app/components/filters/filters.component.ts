c074b78c00030534ac11fe793f0aa7b7dd234d47
KattanMohammad <mohammadsy107@gmail.com>
2025-03-11 09:22:58 +0100

    Amelioration

--- src/app/components/filters/filters.component.ts
+++ src/app/components/filters/filters.component.ts
@@ -14,14 +14,21 @@ export class FiltersComponent {
   categories = Array.from({ length: 10 }, (_, i) => i); // Catégories 0 à 9
   catID = 5;
   fabID!: number;
-  date_debut: string = this.formatDate(new Date()); 
-  date_fin: string = this.formatDate(new Date()); 
+  date_debut: string = this.toInputDate(new Date()); 
+  date_fin: string = this.toInputDate(new Date()); 
 
 
   @Output() filterChange = new EventEmitter < {
     catID: number; fabID: number; date_debut: string;
   date_fin: string; }>();
+  toApiDate(date: string): string {
+    return date.replace(/-/g, ''); // Ex: 2025-03-10 → 20250310
+  }
 
+  // ✅ Convertir Date en YYYY-MM-DD pour <input>
+  toInputDate(date: Date): string {
+    return date.toISOString().split('T')[0]; // Ex: 2025-03-10
+  }
   // ✅ Fonction pour formater la date en YYYYMMDD
   formatDate(date: Date): string {
     const year = date.getFullYear();
@@ -38,18 +45,15 @@ export class FiltersComponent {
   onFabIDChange(): void {
     this.emitChange();
   }
-
+  onDateChange(): void {
+    this.emitChange();
+  }
   emitChange(): void {
     this.filterChange.emit({
-      catID: this.catID, fabID: this.fabID, date_debut: this.date_debut,
-      date_fin:this.date_fin});
+      catID: this.catID,
+      fabID: this.fabID,
+      date_debut: this.toApiDate(this.date_debut), // Format YYYYMMDD
+      date_fin: this.toApiDate(this.date_fin),     // Format YYYYMMDD
+    });
   }
-
-  onDateChange(): void {
-  // Convertir les dates en format YYYYMMDD avant l'envoi
-  this.date_debut = this.formatDate(new Date(this.date_debut));
-  this.date_fin = this.formatDate(new Date(this.date_fin));
-
-  this.emitChange();
-}
 }
