66d9e557916a1528c956bac10c27df3762145328
shyam <shyam1@live.fr>
2025-03-14 10:49:17 +0100

    Revert "storecount et filter separer"
    This reverts commit af6dc1ab58cb9efedba296363e2d8b2f18239ddf.
    
    # Conflicts:
    #	src/app/components/produits/produits.component.html
    #	src/app/components/produits/produits.component.ts

--- src/app/components/produits/produits.component.ts
+++ src/app/components/produits/produits.component.ts
@@ -2,21 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FiltersComponent } from '../filters/filters.component';
 import { GraphComponent } from '../graph/graph.component';
-import { StoreCountComponent } from '../store-count/store-count.component'; // ✅ Ajout
-import { Input } from '@angular/core';  // ✅ Ajout de Input pour récupérer les valeurs de `AppComponent`
 
 @Component({
   selector: 'app-produits',
   standalone: true,
-  imports: [CommonModule, FiltersComponent, GraphComponent,StoreCountComponent],
+  imports: [CommonModule, FiltersComponent, GraphComponent],
   templateUrl: './produits.component.html',
   styleUrls: ['./produits.component.css'],
 })
 export class ProduitsComponent {
-  @Input() catID!: number;  // ✅ Récupère la valeur globale
-  @Input() fabID!: number;
-  @Input() date_debut!: string;
-  @Input() date_fin!: string;
+  catID = 5;
+  fabID!: number;
+  date_debut!: string;
+  date_fin!: string;
 
   updateFilters(filters: { catID: number; fabID: number ,date_debut: string ,date_fin: string}) {
     this.catID = filters.catID;
